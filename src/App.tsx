a21ff0d7a0f52e92734cee67007fbc6756192417
Peter Miles <peter.kincaid@apollo.io>
2024-04-17 14:52:52 -0700

    chore: add css modules, reset css, update page scaffolding (#4)
    * chore: remove unused styles
    
    * feat: add some apollo pizzazz to the logo
    
    * chore: change logo link to apollo tech blog
    
    * fix: update index.html to mention Apollo & use our icon
    
    * chore: enable css modules
    
    * style: update the styling of the app
    
    * fix: remove unnecessary react class name
    
    * chore: add codeowners
    
    * fix: use proper alt tag for apollo logo
    
    * feat: add indicator to where to put your code
    
    Co-authored-by: Sean Malone <sean.malone@apollo.io>
    
    ---------
    
    Co-authored-by: Sean Malone <sean.malone@apollo.io>

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import apolloLogo from "./assets/apollo-logo.jpeg";
-
-import "./App.css";
+import styles from "./App.module.css";
+import classnames from "classnames";
 
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://apollo.io" target="_blank">
-          <img src={apolloLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <div className={styles.root}>
+      <a href="https://www.apollo.io/tech-blog" target="_blank">
+        <img
+          src={apolloLogo}
+          className={classnames(styles.logo)}
+          alt="Apollo logo"
+        />
+      </a>
       <h1>Thanks for interviewing with Apollo!</h1>
-    </>
+      &#8249;Your code here&#8250;
+    </div>
   );
 }
 
